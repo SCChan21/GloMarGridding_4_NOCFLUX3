21c61a349e7986efa4994fa1dbbbd9928ce8aec2
Steven Chan <steven.chan@noc.ac.uk>
2026-03-09 11:06:15 +0000

    Undo

--- glomar_gridding/utils.py
+++ glomar_gridding/utils.py
@@ -810,7 +810,7 @@ def gpu_check():
         True - if GPU exists, False otherwise
     """
     try:
-        subprocess.check_output('nvidia-smi')  # noqa: S607
+        subprocess.check_output('nvidia-smi')
     except Exception:
         return False
     return True
